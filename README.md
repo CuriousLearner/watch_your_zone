a743f06d09a7145bbbc492dac82b5e25a191d70c
CuriousLearner <sanyam.khurana01@gmail.com>
2017-04-22 14:44:40 +0530

    Initial App to visualize data

---
+++ README.md
@@ -0,0 +1,4 @@
+# Watch Your Zone
+
+A hack to help people quickly visualize public health data without manually
+going through thousands of PDFs and other files.
---
+++ src/bubble_chart.js
@@ -0,0 +1,443 @@
+
+
+/* bubbleChart creation function. Returns a function that will
+ * instantiate a new bubble chart given a DOM element to display
+ * it in and a dataset to visualize.
+ *
+ * Organization and style inspired by:
+ * https://bost.ocks.org/mike/chart/
+ *
+ */
+function bubbleChart() {
+  // Constants for sizing
+  var width = 940;
+  var height = 500;
+
+  // tooltip for mouseover functionality
+  var tooltip = floatingTooltip('gates_tooltip', 240);
+
+  // Locations to move bubbles towards, depending
+  // on which view mode is selected.
+  var center = { x: width / 2, y: height / 2 };
+
+  // Used when setting up force and
+  // moving around nodes
+  var damper = 0.102;
+
+  // These will be set in create_nodes and create_vis
+  var svg = null;
+  var bubbles = null;
+  var nodes = [];
+
+  // Charge function that is called for each node.
+  // Charge is proportional to the diameter of the
+  // circle (which is stored in the radius attribute
+  // of the circle's associated data.
+  // This is done to allow for accurate collision
+  // detection with nodes of different sizes.
+  // Charge is negative because we want nodes to repel.
+  // Dividing by 8 scales down the charge to be
+  // appropriate for the visualization dimensions.
+  function charge(d) {
+    return -Math.pow(d.radius, 2.0) / 8;
+  }
+
+  // Here we create a force layout and
+  // configure it to use the charge function
+  // from above. This also sets some contants
+  // to specify how the force layout should behave.
+  // More configuration is done below.
+  var force = d3.layout.force()
+    .size([width, height])
+    .charge(charge)
+    .gravity(-0.01)
+    .friction(0.9);
+
+
+  // Nice looking colors - no reason to buck the trend
+  var fillColor = d3.scale.ordinal()
+    .domain(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+    .range(['#34F060', '#2CCDEF', '#348FFF', '#7AEB2F', '#FAC93B', '#F88248', '#E6F046', '#EE3838', '#F5593B']);
+
+  // Sizes bubbles based on their area instead of raw radius
+  var radiusScale = d3.scale.pow()
+    .exponent(0.5)
+    .range([2, 85]);
+
+  /*
+   * This data manipulation function takes the raw data from
+   * the CSV file and converts it into an array of node objects.
+   * Each node will store data and visualization values to visualize
+   * a bubble.
+   *
+   * rawData is expected to be an array of data objects, read in from
+   * one of d3's loading functions like d3.csv.
+   *
+   * This function returns the new node array, with a node in that
+   * array for each element in the rawData input.
+   */
+  function createNodes(rawData) {
+    // Use map() to convert raw data into node data.
+    // Checkout http://learnjsdata.com/ for more on
+    // working with data.
+
+    var max_val = Math.max.apply(null, rawData.map(function(a) {return a[chart_name]}));
+    console.log("max val", max_val);
+    var myNodes = rawData.map(function (d) {
+      return {
+        id: d.id,
+        radius: scale_x_between_min_and_max(0, max_val, 0, 80, d[chart_name]),
+        value: d[chart_name],
+        name: d.zone,
+        education: d.education,
+        anc: d.anc,
+        contraceptive: d.contraceptive,
+        expense: d.expense,
+        x: Math.random() * 900,
+        y: Math.random() * 800
+      };
+    });
+
+    // sort them to prevent occlusion of smaller nodes.
+    myNodes.sort(function (a, b) { return b.value - a.value; });
+
+    return myNodes;
+  }
+
+  /*
+   * Main entry point to the bubble chart. This function is returned
+   * by the parent closure. It prepares the rawData for visualization
+   * and adds an svg element to the provided selector and starts the
+   * visualization creation process.
+   *
+   * selector is expected to be a DOM element or CSS selector that
+   * points to the parent element of the bubble chart. Inside this
+   * element, the code will add the SVG continer for the visualization.
+   *
+   * rawData is expected to be an array of data objects as provided by
+   * a d3 loading function like d3.csv.
+   */
+  var chart = function chart(selector, rawData) {
+    // Use the max total_amount in the data as the max in the scale's domain
+    // note we have to ensure the total_amount is a number by converting it
+    // with `+`.
+    var maxAmount = d3.max(rawData, function (d) { return +d.total_amount; });
+    radiusScale.domain([0, maxAmount]);
+
+    nodes = createNodes(rawData);
+    // Set the force's nodes to our newly created nodes array.
+    force.nodes(nodes);
+
+    // Create a SVG element inside the provided selector
+    // with desired size.
+    svg = d3.select(selector)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Bind nodes data to what will become DOM elements to represent them.
+    bubbles = svg.selectAll('.bubble')
+      .data(nodes, function (d) { return d[chart_name]; });
+
+    // Create new circle elements each with class `bubble`.
+    // There will be one circle.bubble for each object in the nodes array.
+    // Initially, their radius (r attribute) will be 0.
+    bubbles.enter().append('circle')
+      .classed('bubble', true)
+      .attr('r', 0)
+      .attr('fill', function (d) { return fillColor(d.id); })
+      //.attr('stroke', function (d) { return d3.rgb(fillColor(d.id)).darker(); })
+      .attr('stroke-width', 2)
+      .on('mouseover', showDetail)
+      .on('mouseout', hideDetail);
+
+    // Fancy transition to make bubbles appear, ending with the
+    // correct radius
+    bubbles.transition()
+      .duration(2000)
+      .attr('r', function (d) { return d.radius; });
+
+    // Set initial layout to single group.
+    groupBubbles();
+  };
+
+  /*
+   * Sets visualization in "single group mode".
+   * The year labels are hidden and the force layout
+   * tick function is set to move all nodes to the
+   * center of the visualization.
+   */
+  function groupBubbles() {
+
+    force.on('tick', function (e) {
+      bubbles.each(moveToCenter(e.alpha))
+        .attr('cx', function (d) { return d.x; })
+        .attr('cy', function (d) { return d.y; });
+    });
+
+    force.start();
+  }
+
+  /*
+   * Helper function for "single group mode".
+   * Returns a function that takes the data for a
+   * single node and adjusts the position values
+   * of that node to move it toward the center of
+   * the visualization.
+   *
+   * Positioning is adjusted by the force layout's
+   * alpha parameter which gets smaller and smaller as
+   * the force layout runs. This makes the impact of
+   * this moving get reduced as each node gets closer to
+   * its destination, and so allows other forces like the
+   * node's charge force to also impact final location.
+   */
+  function moveToCenter(alpha) {
+    return function (d) {
+      d.x = d.x + (center.x - d.x) * damper * alpha;
+      d.y = d.y + (center.y - d.y) * damper * alpha;
+    };
+  }
+
+  /*
+   * Sets visualization in "split by year mode".
+   * The year labels are shown and the force layout
+   * tick function is set to move nodes to the
+   * yearCenter of their data's year.
+   */
+  function splitBubbles() {
+
+    force.on('tick', function (e) {
+      bubbles.each(moveToYears(e.alpha))
+        .attr('cx', function (d) { return d.x; })
+        .attr('cy', function (d) { return d.y; });
+    });
+
+    force.start();
+  }
+
+  /*
+   * Helper function for "split by year mode".
+   * Returns a function that takes the data for a
+   * single node and adjusts the position values
+   * of that node to move it the year center for that
+   * node.
+   *
+   * Positioning is adjusted by the force layout's
+   * alpha parameter which gets smaller and smaller as
+   * the force layout runs. This makes the impact of
+   * this moving get reduced as each node gets closer to
+   * its destination, and so allows other forces like the
+   * node's charge force to also impact final location.
+   */
+  function moveToYears(alpha) {
+    return function (d) {
+      var target = yearCenters[d.year];
+      d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
+      d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
+    };
+  }
+
+  /*
+   * Function called on mouseover to display the
+   * details of a bubble in the tooltip.
+   */
+  function showDetail(d) {
+    // change outline to indicate hover state.
+    //d3.select(this).attr('stroke', 'black');
+
+
+    function LightenDarkenColor(col, amt) {
+      console.log(col);
+
+        var usePound = false;
+
+        if (col[0] == "#") {
+            col = col.slice(1);
+            usePound = true;
+        }
+
+        var num = parseInt(col,16);
+
+        var r = (num >> 16) + amt;
+
+        if (r > 255) r = 255;
+        else if  (r < 0) r = 0;
+
+        var b = ((num >> 8) & 0x00FF) + amt;
+
+        if (b > 255) b = 255;
+        else if  (b < 0) b = 0;
+
+        var g = (num & 0x0000FF) + amt;
+
+        if (g > 255) g = 255;
+        else if (g < 0) g = 0;
+
+        return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
+    }
+
+    d3.select(this).attr('fill', LightenDarkenColor(this.attributes.fill.value, 70));
+    //console.log(d);
+    var content = '<span class="name">Zone: </span><span class="value">' +
+                  d.name +
+                  '</span><br/>' +
+                  '<span class="name">' + toTitle(chart_name) +
+                  ': </span><span' +
+                  ' class="value">' +
+                  addCommas(d[chart_name]) +
+                  '</span><br/>';
+    tooltip.showTooltip(content, d3.event);
+  }
+
+  function toTitle (str) {
+    if ((str===null) || (str===''))
+         return false;
+    else
+     str = str.toString();
+   return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+  /*
+   * Hides tooltip
+   */
+  function hideDetail(d) {
+    // reset outline
+    d3.select(this)
+      //.attr('stroke', d3.rgb(fillColor(d.id)).darker())
+      .attr('fill', d3.rgb(fillColor(d.id)));
+
+    tooltip.hideTooltip();
+  }
+
+  /*
+   * Externally accessible function (this is attached to the
+   * returned chart function). Allows the visualization to toggle
+   * between "single group" and "split by year" modes.
+   *
+   * displayName is expected to be a string and either 'year' or 'all'.
+   */
+  chart.toggleDisplay = function (displayName) {
+    if (displayName === 'anc') {
+      chart_name = 'anc';
+      desc.innerHTML = "Percentage of women who had at least 4 Antenatal" +
+          " Care visits.";
+      refresh_display();
+
+      //splitBubbles();
+      //bubbles = d3.selectAll('.bubble')
+      //  .data(nodes, function (d) { return d[chart_name]; });
+    }
+
+    else if (displayName == 'contraceptive') {
+      chart_name = 'contraceptive';
+      desc.innerHTML = "Total need for family planning";
+      refresh_display();
+    }
+
+    else if (displayName == 'expense') {
+      chart_name = 'expense';
+      desc.innerHTML = "Total out of pocket expenditure in a public hospital";
+      refresh_display();
+    }
+
+    else {
+      chart_name = "education";
+      desc.innerHTML = "Percentage of women with 10+ years of education.";
+      refresh_display();
+      //groupBubbles();
+      //bubbles = d3.selectAll('.bubble')
+      //  .data(nodes, function (d) { return d[chart_name]; });
+    }
+  };
+
+
+  // return the chart function from closure.
+  return chart;
+}
+
+/*
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
+
+var myBubbleChart = bubbleChart();
+
+/*
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
+function display(error, data) {
+  if (error) {
+    console.log(error);
+  }
+
+  myBubbleChart('#vis', data);
+}
+
+/*
+ * Sets up the layout buttons to allow for toggling between view modes.
+ */
+function setupButtons() {
+  d3.select('#toolbar')
+    .selectAll('.button')
+    .on('click', function () {
+      // Remove active class from all buttons
+      d3.selectAll('.button').classed('active', false);
+      // Find the button just clicked
+      var button = d3.select(this);
+
+      // Set it as the active button
+      button.classed('active', true);
+
+      // Get the id of the button
+      var buttonId = button.attr('id');
+
+      // Toggle the bubble chart based on
+      // the currently clicked button.
+      myBubbleChart.toggleDisplay(buttonId);
+    });
+}
+
+/*
+ * Helper function to convert a number into a string
+ * and add commas to it to improve presentation.
+ */
+function addCommas(nStr) {
+  nStr += '';
+  var x = nStr.split('.');
+  var x1 = x[0];
+  var x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+
+  return x1 + x2;
+}
+
+// Load the data.
+d3.csv('data/data.csv', display);
+
+// setup the buttons.
+setupButtons();
+
+function scale_x_between_min_and_max(min, max, a, b, x) {
+  return (((b - a) * (x - min)) / (max - min)) + a;
+}
+
+var chart_name = 'education';
+var desc = document.getElementById('desc');
+desc.innerHTML = "Percentage of women with 10+ years of education.";
+
+var refresh_display = function() {
+  var vis = document.getElementById("vis");
+  vis.childNodes[0].outerHTML = "";
+  delete vis.childNodes[0];
+  d3.csv('data/data.csv', display);
+};
+
+d3.select("body")
+  .style("background-color", "#3e047e");
+
+var my_array;
